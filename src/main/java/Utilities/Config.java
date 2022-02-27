54b446cb9916e8aad02a9a1b60803799ef668059
krishnasai1907 <57449497+krishnasai1907@users.noreply.github.com>
2022-02-27 11:20:06 +0530

    cong

--- src/main/java/Utilities/Config.java
+++ src/main/java/Utilities/Config.java
@@ -7,7 +7,7 @@ public class Config {
 	static Properties prop;
 	public static String getProperties(String locName)
 	{
-		 prop=new Properties();
+		 Prop=new Properties();
 		 try
 		 {
 			 String splitted=locName.split("\\.")[0].toString();
