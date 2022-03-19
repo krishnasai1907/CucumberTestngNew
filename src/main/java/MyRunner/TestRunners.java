744c78e4e3602e0addd3c4e234df607cc34f7600
krishnasai1907 <57449497+krishnasai1907@users.noreply.github.com>
2022-03-19 17:18:41 +0530

    updated sample

--- src/main/java/MyRunner/TestRunners.java
+++ src/main/java/MyRunner/TestRunners.java
@@ -9,6 +9,8 @@ import Utilities.Utility;
 import io.cucumber.java.Before;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = ".\\Features"  /* U can use ./Features or .\\Features anything  will work*/
@@ -44,4 +46,13 @@ public class TestRunners extends AbstractTestNGCucumberTests {
 	    public Object[][] scenarios() {
 	        return super.scenarios();
 	    }
+
+		@Parameters("browser")
+		@Test
+		public void checking(String checkingtest)
+		{
+			System.out.println(checkingtest);
+			System.out.println(checkingtest);
+			System.out.println(checkingtest);
+		}
 }
